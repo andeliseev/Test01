d68dafc787bd5045255f01bea469858fd1a11478
Andrei Eliseev <a.eliseev@kefirgames.com>
2024-05-05 16:16:57 +0300

    Updates

--- assets/batchSamplersUniformGroup-pNoNzgi0.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./index-On9aXHkL.js";import{U as o}from"./colorToUniform-E1ZIR89n.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
